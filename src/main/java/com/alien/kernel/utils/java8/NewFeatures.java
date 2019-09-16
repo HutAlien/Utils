7d86fb29955fb220c7c020e2b39ca818f4f881d8
fengyunjun <fengyunjun@bonc.com.cn>
2019-09-16 14:52:09 +0800

    about optional

---
+++ src/main/java/com/alien/kernel/utils/java8/NewFeatures.java
@@ -0,0 +1,35 @@
+package com.alien.kernel.utils.java8;
+
+import com.alien.kernel.entity.Employee;
+import com.alien.kernel.utils.StringUtils;
+
+import java.util.Optional;
+
+/**
+ * @Auther: FYJ
+ * @Date: 2019/9/16 14:07
+ * @Description:
+ */
+public class NewFeatures {
+
+    private  String name;
+
+    private static String getInfo(Employee employee) {
+        //Optional<Employee> optional=Optional.ofNullable(employee); 创建一个optional对象，不允许为空,为空则返回空
+        //Optional.of() 创建一个optional对象，为空则抛出异常
+       return Optional.ofNullable(employee).map((e)->e.getName()).orElse("name is null");
+    }
+
+    private void set(String name){
+        this.name=Optional.ofNullable(name).filter(StringUtils::isTranslate).orElseThrow(()->new IllegalArgumentException());
+    }
+
+    public static void main(String[] args) {
+        Employee employee=new Employee();
+        employee.setName("Tom");
+        System.out.println(getInfo(employee));
+        //
+        NewFeatures newFeatures=new NewFeatures();
+        newFeatures.set(null);
+    }
+}
