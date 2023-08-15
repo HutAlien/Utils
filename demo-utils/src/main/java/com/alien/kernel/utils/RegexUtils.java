18b21a9cd7dbddb0d683bd86c52c62c4b031a5b3
fengyunjun <feng.yunjun@foundbyte.com>
2023-08-15 17:35:40 +0800

    正则表达式

--- demo-utils/src/main/java/com/alien/kernel/utils/RegexUtils.java
+++ demo-utils/src/main/java/com/alien/kernel/utils/RegexUtils.java
@@ -19,7 +19,15 @@ public class RegexUtils {
      * 正则表达式：验证座机号
      */
     public static final String REGEX_TEL = "^(((\\d{3,4}-)?[0-9]{7,8})|(1(3|4|5|6|7|8|9)\\d{9}))$";
-    ;
+    /**
+     * 匹配正常11位手机|11+4的隐私号
+     */
+    public static final String REGEX_CUSTOMER_PHONE_IMPORT="^1[3456789]\\d{9}-\\d{4}$|^1[3456789]\\d{9}$";
+    /**
+     * 汉字，字母数字下划线以及指定符号：-!@#¥%*&^$()
+     */
+    public static final String REGEX_CUSTOMER_NAME_IMPORT="^[\\u4e00-\\u9fa5_\\-!@#¥%*&^$()\\[\\]?:a-zA-Z0-9]+$";
+
 
     public static void main(String[] args) {
         System.out.println(Pattern.matches(REGEX_TEL,"(0571)28059570"));
